a4941d96051247500a2786df4a99ce52dc6d8f25
militoj <militoj@gmail.com>
2019-01-05 14:00:35 -0600

    buy a new one

--- src/main/java/com/example/JanuaryDemo/HelloWorld.java
+++ src/main/java/com/example/JanuaryDemo/HelloWorld.java
@@ -34,7 +34,10 @@ public class HelloWorld {
     @RequestMapping("/fuckyocouch")
     public String fuckyocouch() {
         return
-               " <h1> butt </h1> ";
+               " <h1> Buy a new one you rich motherfucker </h1> " +
+                       "<img src=https://media0.giphy.com/media/DOb3rFL6d83Zu/giphy.gif?cid=3640f6095c310c4c5a722e306bf4d80c alt=\"This is an animated gif image, but it does not move\"/>\n"
+
+                ;
 
 
     }
