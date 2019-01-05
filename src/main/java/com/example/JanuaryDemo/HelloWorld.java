c51b1cf47bcc9c95e88607a5b10602cabf899218
militoj <militoj@gmail.com>
2019-01-05 15:26:32 -0600

    addred request_param

--- src/main/java/com/example/JanuaryDemo/HelloWorld.java
+++ src/main/java/com/example/JanuaryDemo/HelloWorld.java
@@ -3,9 +3,7 @@ package com.example.JanuaryDemo;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -35,12 +33,28 @@ public class HelloWorld {
     public String fuckyocouch() {
         return
                " <h1> Buy a new one you rich motherfucker </h1> " +
-                       "<img src=https://media0.giphy.com/media/DOb3rFL6d83Zu/giphy.gif?cid=3640f6095c310c4c5a722e306bf4d80c alt=\"This is an animated gif image, but it does not move\"/>\n"
+                       "<br> <img src=https://media0.giphy.com/media/DOb3rFL6d83Zu/giphy.gif?cid=3640f6095c310c4c5a722e306bf4d80c alt=\"This is an animated gif image, but it does not move\"/>\n" +
+                      "<br> <br> <button type=button>Couch Store</button> "
 
-                ;
+
+        ;
 
 
     }
+
+    // use link from
+    //http://localhost:8080/fuckyocouchname?bitchname=Jemery
+    @RequestMapping(value = "/fuckyocouchname")
+    @ResponseBody
+    public String fuckyocouchname(@RequestParam("bitchname") String bitchname){
+        return " <h1> Buy a new one " + bitchname + ", you rich motherfucker </h1> " +
+                "<br> <img src=https://media0.giphy.com/media/DOb3rFL6d83Zu/giphy.gif?cid=3640f6095c310c4c5a722e306bf4d80c alt=\"This is an animated gif image, but it does not move\"/>\n" +
+                "<br> <br> <button type=button>Couch Store</button> "
+                ;
+
+    }
+
+
 
 
 
